ff0a6d24837bc7847f232c13dfc1eb0bd3479a00
AbanoubSamir3004 <bebeoperns3004@gmail.com>
2022-07-04 14:57:03 +0200

    a

--- assets/js/js.js
+++ assets/js/js.js
@@ -23,9 +23,29 @@ $(document).ready(function () {
     loop: true,
     items: 6,
     margin: 10,
-    autoplay: true,
+    // autoplay: true,
     autoplayTimeout: 1000,
     autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      900: {
+        items: 4,
+      },
+      1100: {
+        items: 5,
+      },
+      1300: {
+        items: 6,
+      },
+    },
   });
 });
 
